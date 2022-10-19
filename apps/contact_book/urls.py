487426baf06c67c42c89a6e854785f7ffab60538
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-19 23:43:22 +0200

    crud views urls creation

--- apps/contact_book/urls.py
+++ apps/contact_book/urls.py
@@ -11,4 +11,9 @@ app_name = "contact_book"
 urlpatterns = [
     # get_contacts : name of our view
     path("", views.get_contact_book, name="contact_book"),
+    path("", views.create_contact, name="create_contact"),
+    path("<int:pk>/edit", views.edit_contact, name="edit_contact"),
+    path("<int:pk>/delete", views.delete_contact, name="delete_contact"),
+    path("<int:pk>/read", views.read_contact, name="read_contact"),
+    path("create/", views.create_contact, name="create_contact"),
 ]
