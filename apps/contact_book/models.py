a875fc0d6a2ba3308b7f5e1cc4943d7c93772980
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-19 23:35:17 +0200

    model update

--- apps/contact_book/models.py
+++ apps/contact_book/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Contact_book(models.Model):
     name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=20)
     birthday = models.DateField()
 
     created_at = models.DateTimeField(auto_now_add=True)
