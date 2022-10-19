bf0fd98296e63487177b2db4679c07b920429836
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-19 23:42:51 +0200

    crud views and their templates creation

--- apps/contact_book/views.py
+++ apps/contact_book/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
+from .forms import ContactForm
 from .models import Contact_book
 
 
@@ -17,3 +18,62 @@ def get_contact_book(request: HttpRequest) -> HttpResponse:
             "title": "Contact_book",
         },
     )
+
+
+# after updating it will redirect to detail_View
+def detail_view(request: HttpRequest, pk) -> HttpResponse:
+    # dictionary for initial data with
+    # field names as keys
+    context = {"data": Contact_book.objects.get(pk=pk)}
+    # add the dictionary during initialization
+    return render(request, "update_view.html", context)
+
+
+def edit_contact(request: HttpRequest, pk) -> HttpResponse:
+    context = {}
+    obj = get_object_or_404(Contact_book, pk=pk)
+    form = ContactForm(request.POST or None, instance=obj)
+    if form.is_valid():
+        form.save()
+    context["form"] = form
+    return render(request, "edit.html", context)
+
+
+def read_contact(request: HttpRequest, pk: int) -> HttpResponse:
+    contact = Contact_book.objects.get(pk=pk)
+    form = ContactForm(instance=contact)
+    return render(
+        request,
+        "read.html",
+        {
+            # contacts in general will be refering
+            # to generator fuc , but here to forms in this fun upper (with objects)
+            "form": form,
+            "title": "Read_contact",
+        },
+    )
+
+
+def delete_contact(request: HttpRequest, pk: int) -> HttpResponse:
+    contact = Contact_book.objects.get(pk=pk)
+    contact.delete()
+    return render(
+        request,
+        "delete.html",
+        {
+            # contacts in general will be refering
+            # to generator fuc , but here to forms in this fun upper (with objects)
+            "title": "Delete_contact",
+        },
+    )
+
+
+def create_contact(request: HttpRequest) -> HttpResponse:
+    context = {}
+    # add the dictionary during initialization
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+    context["form"] = form
+    return render(request, "create.html", context)
