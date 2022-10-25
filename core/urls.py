ccaed966a7e2832a5c4eabeee03c3d91e0c641be
Click1N <111238061+Click1N@users.noreply.github.com>
2022-10-25 12:54:09 +0200

    sessions_example app linked to core

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.contact_book.urls")),
+    path("sessions-example/", include("apps.sessions_example.urls")),
 ]
 
 #    path("admin/", admin.site.urls),
