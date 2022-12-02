5558e3162535c447e25ed8f6518e8c09299722ab
Click1N <111238061+Click1N@users.noreply.github.com>
2022-12-02 12:16:14 +0100

    add limitation on user rights

--- apps/contact_book/urls.py
+++ apps/contact_book/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
@@ -12,9 +13,11 @@ urlpatterns = [
     # get_contacts : name of our view
     path("", views.get_contact_book, name="contact_book"),
     path("", views.create_contact, name="create_contact"),
-    path("<int:pk>/edit", views.edit_contact, name="edit_contact"),
-    path("<int:pk>/delete", views.delete_contact, name="delete_contact"),
+    path("<int:pk>/edit", login_required(views.edit_contact), name="edit_contact"),
+    path(
+        "<int:pk>/delete", login_required(views.delete_contact), name="delete_contact"
+    ),
     path("<int:pk>/read", views.read_contact, name="read_contact"),
-    path("create/", views.create_contact, name="create_contact"),
+    path("create/", login_required(views.create_contact), name="create_contact"),
     #   path("<int:pk>", views.ContactUpdateView.as_view(), name="edit"),
 ]
