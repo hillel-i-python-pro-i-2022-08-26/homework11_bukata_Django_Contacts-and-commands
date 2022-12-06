e68b91e2eae520dfcf573e4b1fea4af0dbeee5ac
Click1N <111238061+Click1N@users.noreply.github.com>
2022-12-06 18:34:31 +0100

    new model CustomUser created as well as form for it

--- apps/contact_book/forms.py
+++ apps/contact_book/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import SelectDateWidget
 
-from apps.contact_book.models import Contact_book
+from apps.contact_book.models import Contact_book, CustomUser
 
 
 class ContactForm(forms.ModelForm):
@@ -15,3 +15,14 @@ class ContactForm(forms.ModelForm):
         # to see one spesific field or couple for exmp
 
         fields = ("name", "phone", "birthday", "avatar")
+
+
+class CustomUserForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        # to show all fields in the form
+        fields = "__all__"
+
+        # to see one spesific field or couple for exmp
+
+    #    fields = ("name", "email", "password", "avatar")
